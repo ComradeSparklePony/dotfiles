774c432fbf08acb55364ec6e9d5522d67ca9c62e
Comrade SparklePony <comradesparklepony@gmail.com>
2019-04-25 17:50:07 -0400

    add screen layouts

---
+++ .screenlayout/2scree.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output DP1 --mode 1600x900 --pos 1680x0 --rotate normal --output eDP1 --primary --mode 1680x1050 --pos 0x0 --rotate normal --output VIRTUAL1 --off
